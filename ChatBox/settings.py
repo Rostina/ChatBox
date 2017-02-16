f8aaad9314054b53710aa99326f7f9c0f57ef19c
yyfastow <yoseffastow@gmail.com>
2017-02-16 16:12:16 -0500

    Add Procfile.txt

--- ChatBox/settings.py
+++ ChatBox/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = (
     os.path.join(SITE_ROOT, 'static'),
 )
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "staticfiles")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --pythonpath ChatBox ChatBox.wsgi --log-file -
