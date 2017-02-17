d3557abda9f8c8f3f35278060e1d98d47f66c18a
yyfastow <yoseffastow@gmail.com>
2017-02-16 20:30:05 -0500

    Added Procfile.windows

--- ChatBox/settings.py
+++ ChatBox/settings.py
@@ -27,7 +27,6 @@ def get_env_variables(var_name):
             raise ImproperlyConfigured(error_msg)
 
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
