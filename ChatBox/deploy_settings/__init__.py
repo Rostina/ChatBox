f9b20da3e3e35694f4c7e4b5aa47fb657241643d
yyfastow <yoseffastow@gmail.com>
2017-02-16 22:34:21 -0500

    Add whitenoise to project

--- ChatBox/deploy_settings/__init__.py
+++ ChatBox/deploy_settings/__init__.py
@@ -14,3 +14,5 @@ SECRET_KEY = get_env_variables("SECRET_KEY")
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
+
+ARTIFILES_STORAGE = "whitenoise.django.GzipManfestStaticFileStorage"
