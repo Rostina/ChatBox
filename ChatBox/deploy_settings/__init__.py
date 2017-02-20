fa0221835a277800904e42010186fcae6817dc77
yyfastow <yoseffastow@gmail.com>
2017-02-19 19:16:27 -0500

    Added AWS link for deploy settings

--- ChatBox/deploy_settings/__init__.py
+++ ChatBox/deploy_settings/__init__.py
@@ -17,10 +17,13 @@ DATABASES['default'].update(db_from_env)
 
 ARTIFILES_STORAGE = "whitenoise.django.GzipManfestStaticFileStorage"
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'yyf-chatbox-bucket'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_URL = "https://yyf-chatbox-bucket.s3.amazonaws.com/"
-
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
