f5e08850413868370f0457455354ffac3aff4b84
yyfastow <yoseffastow@gmail.com>
2017-02-20 13:41:48 -0500

    Added AWS link for deploy settings

--- ChatBox/deploy_settings/__init__.py
+++ ChatBox/deploy_settings/__init__.py
@@ -38,12 +38,13 @@ DATABASES['default'].update(db_from_env)
 
 AWS_ACCESS_KEY_ID = 'AKIAIOSS67XBV6I5ZIBA'
 AWS_SECRET_ACCESS_KEY = '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'
+# AWS_STORAGE_BUCKET_NAME = 'yyf-chatbox-bucket'
 
 AWS_FILE_EXPIRE = 200
 AWS_PRELOAD_METADATA = True
 AWS_QUERYSTRING_AUTH = True
 
-DEFAULT_FILE_STORAGE = 'ChatBoxutils.MediaRootS3BotoStorage'
+DEFAULT_FILE_STORAGE = 'ChatBox.utils.MediaRootS3BotoStorage'
 STATICFILES_STORAGE = 'ChatBox.utils.StaticRootS3BotoStorage'
 AWS_STORAGE_BUCKET_NAME = 'yyf-chatbox-bucket'
 S3DIRECT_REGION = 'us-west-2'
