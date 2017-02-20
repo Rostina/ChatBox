4466f6c102f8b48b3bf39c2455e980ce857b1720
yyfastow <yoseffastow@gmail.com>
2017-02-20 14:16:59 -0500

    a

--- ChatBox/deploy_settings/__init__.py
+++ ChatBox/deploy_settings/__init__.py
@@ -43,7 +43,7 @@ DATABASES['default'].update(db_from_env)
 AWS_QUERYSTRING_AUTH = False
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
+AWS_STORAGE_BUCKET_NAME = 'yyf-chatbox-bucket'
 MEDIA_URL = 'http://%s.s3.amazonaws.com/your-folder/' % AWS_STORAGE_BUCKET_NAME
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
 
