9d0769804553cf367afe44ecc391ac8ecbc01f54
yyfastow <yoseffastow@gmail.com>
2017-02-19 23:08:19 -0500

    Added AWS link for deploy settings

--- ChatBox/deploy_settings/__init__.py
+++ ChatBox/deploy_settings/__init__.py
@@ -27,15 +27,15 @@ AWS_SECRET_ACCESS_KEY = 'NrLGTcqtoTYtJLfn1QD85evvmio46/r9BeO2EELk'
 
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-# STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
-# STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 # ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
-STATICFILES_LOCATION = 'static'
-STATICFILES_STORAGE = custom_storages.StaticStorage
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+#STATICFILES_LOCATION = 'static'
+#STATICFILES_STORAGE = custom_storages.StaticStorage
+#STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
 
-MEDIAFILES_LOCATION = 'media'
-MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
-DEFAULT_FILE_STORAGE = custom_storages.MediaStorage
+#MEDIAFILES_LOCATION = 'media'
+#MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+#DEFAULT_FILE_STORAGE = custom_storages.MediaStorage
